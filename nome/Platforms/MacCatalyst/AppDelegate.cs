e2d658d32d7df190890292416c05b81ebda10568
uilifer <alifersilva011@gmai.com>
2024-09-16 09:04:01 -0300

    jabibis delicia

---
+++ nome/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace nome;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ nome/questau.cs
@@ -0,0 +1,14 @@
+namespace nome;
+
+public class questau;
+
+{
+public void desenha()
+{
+
+}
+
+public string resposta1; {get;set;}
+public string resposta2; {get;set;}
+public string resposta3; {get;set;}
+}
