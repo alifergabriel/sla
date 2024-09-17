5047e8e2333726fb35b7ea49041da9fe5b05e7ce
alifer <alifersilva011@gmail.com>
2024-09-17 08:58:14 -0300

    jabibis delicia

--- nome/questau.cs
+++ nome/questau.cs
@@ -2,13 +2,39 @@ namespace nome;
 
 public class questau;
 
-{
-public void desenha()
-{
+public string RespostaErrada {get; set;}
+public string RespostaCorreta {get; set;}
+public string Pergunta {get; set;}
 
+private button QualBTN(int rr)
+{
+    if(rr == 1)
+    return BotaoResposta01;
+    else if (rr == 2)
+    return BotaoResposta02;
+    else if (rr == 3)
+    return BotaoResposta03;  
+    else if (rr == 4)
+    return BotaoResposta04;
+    else if (rr == 5)
+    return BotaoResposta05;
+    else
+    return null;
 }
-
-public string resposta1; {get;set;}
-public string resposta2; {get;set;}
-public string resposta3; {get;set;}
+public bool verificaResposta(int rr)
+{
+    if (RespostaCerta == rr)
+    {
+        var BTN = QualBTN(rr);
+        BTN BackgroundColor = Colors.Green;
+        return true;
+    }
+    else
+    {
+        var BTNCorreto = QualBTN(RespostaCerta);
+        var BTNIncorreto = QualBTN(rr);
+        BTNCorreto.BackgroundColor = Colors.Purple;
+         BTNIncorreto.BackgroundColor = Colors.Red;
+         return false;
+    }
 }
